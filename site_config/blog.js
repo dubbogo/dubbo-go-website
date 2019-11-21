ed4dfe0ee3b68da4318f286272c387ed43708036
Ming Deng <mindeng@ebay.com>
2019-11-21 09:53:50 +0800

    Add Graceful shutdown blog

--- site_config/blog.js
+++ site_config/blog.js
@@ -22,7 +22,14 @@ export default {
                 dateStr: 'Nov 10th, 2019',
                 desc: '在dubbogo中的TPS limit的设计与实现。讨论了目前已有的实现，以及可以扩展的接口。',
                 link: '/zh-cn/blog/dubbogo-tps-limit-design-implementation.html',
-            }
+            },
+            {
+                title: '要优雅退出吗？dubbogo给你',
+                author: '@flycash',
+                dateStr: 'Nov 21th, 2019',
+                desc: '在dubbogo中的优雅退出的设计与实现。讨论了直接退出的存在问题，因此得出优雅退出所需的步骤。',
+                link: '/zh-cn/blog/graceful-shutdown.html',
+            },
         ]
     },
 };
